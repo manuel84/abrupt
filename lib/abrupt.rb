75d69552d7bc355469d0290f1256bbe67935471a
Manuel Dudda <dudda@redpeppix.de>
2014-11-04 17:13:43 +0100

    [Voc] SWRL rules removed

--- lib/abrupt.rb
+++ lib/abrupt.rb
@@ -10,13 +10,14 @@ module Abrupt
   end
 
   def self.crawl(uri, *args)
-    crawler = Abrupt::Crawler.new uri, args.first
+    opts = args.first
+    crawler = Abrupt::Crawler.new uri, opts
     start_time = Time.now
     Abrupt.log "begin: #{start_time}\n"
     result = crawler.crawl
     end_time = Time.now
     Abrupt.log "\nfinished in #{(end_time - start_time).round} sec.\n\n"
-    case args[:format]
+    case opts[:format]
     when 'xml'
       puts Abrupt::Converter.xml(result)
     else # owl as default
