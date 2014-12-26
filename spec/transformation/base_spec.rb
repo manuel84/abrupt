db49f115c7130ff856ce73c1c973463dc9651014
Manuel Dudda <dudda@redpeppix.de>
2014-12-26 13:16:26 +0100

    [Refactoring] better specs (shared_example + it_should_behave_like)

--- spec/transformation/base_spec.rb
+++ spec/transformation/base_spec.rb
@@ -1,74 +1,48 @@
 require 'spec_helper'
 
-describe Abrupt::Transformation::Base do
+shared_examples 'transformable object' do |key, value, expected_transformation, type|
   let(:values) do
     values = {
         base: {
             example: {
                 key: {
                     string_value: 'hallo',
-                    float_value: '3.14',
-                    integer_value: '-2',
                     boolean_value: 'present',
-                    array_string_values: %w(hallo Welt),
-                    array_float_values: ['1.4', 3],
-                    array_float_values: [-3, '1'],
-                    array_boolean_values: [true, false, nil, 'present']
+                    integer_value: '-2',
+                    float_value: '3.14',
+                    array_string_values: ['hallo', 'Welt', 3],
+                    array_boolean_values: [true, false, nil, 'present'],
+                    array_integer_values: [-3, '1'],
+                    array_float_values: ['1.4', 3]
                 },
                 simple_string: 'tada'
             }
         }
     }
   end
+  it "the reference is transformed to #{key}" do
+    subject.update_value([:example, :key, key], value, {type: type})
+    expect(subject.values[:base][:example][:key][key]).to eql(expected_transformation)
+    subject.values[:base][:example][:key].delete key
+    values[:base][:example][:key].delete key
+    expect(subject.values).to eql(values)
+  end
+end
+
+describe Abrupt::Transformation::Base do
+
   let(:subject) do
     website = %w(Website http://www.dudda-und-dudda.de)
     page = %w(Page kontakt)
     Abrupt::Transformation::Base.new(website, page, values.dup)
   end
-  context '#update_value' do
-    context 'array values' do
-      it 'transforms the references to strings' do
-      end
-      it 'transforms the references to booleans' do
+  it_should_behave_like 'transformable object', :string_value, 'Welt', 'Welt', :string
+  it_should_behave_like 'transformable object', :boolean_value, 'present', true, :boolean
+  it_should_behave_like 'transformable object', :integer_value, '-2', -2, :integer
+  it_should_behave_like 'transformable object', :float_value, '3.14', 3.14, :number
+  it_should_behave_like 'transformable object', :array_string_values, ['hallo', 'Welt', 3], %w(hallo Welt 3), :string
+  it_should_behave_like 'transformable object', :array_boolean_values, [true, false, nil, 'present'], Array.new(4, true), :boolean
+  it_should_behave_like 'transformable object', :array_integer_values, [-3, '1'], [-3, 1], :integer
+  it_should_behave_like 'transformable object', :array_float_values, ['1.4', 3], [1.4, 3.0], :number
 
-      end
-      it 'transforms the references to integers' do
-
-      end
-      it 'transforms the references to floats' do
-
-      end
-    end
-    context 'single value' do
-      it 'transforms the reference to a string' do
-        subject.update_value([:example, :key, :string_value], 'Welt', {type: :string})
-        expect(subject.values[:base][:example][:key][:string_value]).to eql('Welt')
-        subject.values[:base][:example][:key].delete :string_value
-        values[:base][:example][:key].delete :string_value
-        expect(subject.values).to eql(values)
-      end
-      it 'transforms the reference to a boolean' do
-        subject.update_value([:example, :key, :boolean_value], 'present', {type: :boolean})
-        expect(subject.values[:base][:example][:key][:boolean_value]).to be_truthy
-        subject.values[:base][:example][:key].delete :boolean_value
-        values[:base][:example][:key].delete :boolean_value
-        expect(subject.values).to eql(values)
-      end
-      it 'transforms the reference to a integer' do
-        subject.update_value([:example, :key, :integer_value], -2, {type: :integer})
-        expect(subject.values[:base][:example][:key][:integer_value]).to eql(-2)
-        subject.values[:base][:example][:key].delete :integer_value
-        values[:base][:example][:key].delete :integer_value
-        expect(subject.values).to eql(values)
-      end
-      it 'transforms the reference to a float' do
-        subject.update_value([:example, :key, :float_value], '3.14', {type: :number})
-        expect(subject.values[:base][:example][:key][:float_value]).to eql(3.14)
-        subject.values[:base][:example][:key].delete :float_value
-        values[:base][:example][:key].delete :float_value
-        expect(subject.values).to eql(values)
-      end
-    end
-
-  end
 end
