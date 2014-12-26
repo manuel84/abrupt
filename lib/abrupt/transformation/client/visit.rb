3a3780d6b85e14ae2f9b4073867c8320f17a3811
Manuel Dudda <dudda@redpeppix.de>
2014-12-26 13:35:22 +0100

    [Refactoring] rubocop

--- lib/abrupt/transformation/client/visit.rb
+++ lib/abrupt/transformation/client/visit.rb
@@ -5,10 +5,10 @@ module Abrupt
       # Transformation clas for client visit data
       class Visit < Base
         def initialize(domain, page, visitor)
-          time_format = '%d/%b/%Y:%H:%M:%S'
-          time_output_format = '%Y-%m-%d_%H%M%S'
-          url_time = Time.strptime(visitor.css('entertime').text, time_format)
-                         .strftime(time_output_format)
+          input_format = '%d/%b/%Y:%H:%M:%S'
+          output_format = '%Y-%m-%d_%H%M%S'
+          url_time = Time.strptime(visitor.css('entertime').text, input_format)
+                         .strftime(output_format)
           super(['Website', domain, 'Page', domain + page.css('uri').text],
                 ['Visit', visitor.css('ip').text, 'Time', url_time])
         end
--- spec/transformation/base_spec.rb
+++ spec/transformation/base_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-shared_examples 'transformable object' do |key, value, expected_transformation, type|
+shared_examples 'transformable object' do |key, value, expected, type|
   let(:values) do
-    values = {
+    {
         base: {
             example: {
                 key: {
@@ -21,8 +21,8 @@ shared_examples 'transformable object' do |key, value, expected_transformation, 
     }
   end
   it "the reference is transformed to #{key}" do
-    subject.update_value([:example, :key, key], value, {type: type})
-    expect(subject.values[:base][:example][:key][key]).to eql(expected_transformation)
+    subject.update_value([:example, :key, key], value, type: type)
+    expect(subject.values[:base][:example][:key][key]).to eql(expected)
     subject.values[:base][:example][:key].delete key
     values[:base][:example][:key].delete key
     expect(subject.values).to eql(values)
@@ -36,13 +36,21 @@ describe Abrupt::Transformation::Base do
     page = %w(Page kontakt)
     Abrupt::Transformation::Base.new(website, page, values.dup)
   end
-  it_should_behave_like 'transformable object', :string_value, 'Welt', 'Welt', :string
-  it_should_behave_like 'transformable object', :boolean_value, 'present', true, :boolean
-  it_should_behave_like 'transformable object', :integer_value, '-2', -2, :integer
-  it_should_behave_like 'transformable object', :float_value, '3.14', 3.14, :number
-  it_should_behave_like 'transformable object', :array_string_values, ['hallo', 'Welt', 3], %w(hallo Welt 3), :string
-  it_should_behave_like 'transformable object', :array_boolean_values, [true, false, nil, 'present'], Array.new(4, true), :boolean
-  it_should_behave_like 'transformable object', :array_integer_values, [-3, '1'], [-3, 1], :integer
-  it_should_behave_like 'transformable object', :array_float_values, ['1.4', 3], [1.4, 3.0], :number
+  it_should_behave_like 'transformable object', :string_value,
+                        'Welt', 'Welt', :string
+  it_should_behave_like 'transformable object', :boolean_value,
+                        'present', true, :boolean
+  it_should_behave_like 'transformable object', :integer_value,
+                        '-2', -2, :integer
+  it_should_behave_like 'transformable object', :float_value,
+                        '3.14', 3.14, :number
+  it_should_behave_like 'transformable object', :array_string_values,
+                        ['hallo', 'Welt', 3], %w(hallo Welt 3), :string
+  it_should_behave_like 'transformable object', :array_boolean_values,
+                        [true, false, nil, 'present'], [true] * 4, :boolean
+  it_should_behave_like 'transformable object', :array_integer_values,
+                        [-3, '1'], [-3, 1], :integer
+  it_should_behave_like 'transformable object', :array_float_values,
+                        ['1.4', 3], [1.4, 3.0], :number
 
 end
