9fdd647413ba36160152e03bce11a96610152e6c
Manuel Dudda <dudda@redpeppix.de>
2015-02-23 15:33:52 +0100

    [Fix] logic failure

--- lib/abrupt/transformation/client/visit.rb
+++ lib/abrupt/transformation/client/visit.rb
@@ -56,7 +56,7 @@ module Abrupt
         def add_visit_duration
           leavetime = Abrupt.parse_time(@values[:leavetime])
           enterteime = Abrupt.parse_time(@values[:entertime])
-          return if leavetime && enterteime # maybe no recognized leavetime
+          return unless leavetime && enterteime # maybe no recognized leavetime
           visit_duration = (leavetime - enterteime).to_f # in seconds
           add_data_property('visitDuration', visit_duration)
         end
